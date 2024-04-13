3fc908b6044af8792c00ccf70588ff93bff8d4f3
pramirezd <pablo.ramirez.fcfm@gmail.com>
2024-04-12 23:07:16 -0400

    Carga bootstrap y configuracion navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <Link to="/"> Home </Link> | <Link to="/favoritos"> Favoritos </Link>
+    <nav className="navbar navbar-expand text-center">
+      <div className="container-fluid grid justify-content-center">
+        <Link to="/"> Home </Link>
+        &nbsp;|&nbsp;
+        <Link to="/favoritos"> Favoritos </Link>
+      </div>
     </nav>
   );
 };
